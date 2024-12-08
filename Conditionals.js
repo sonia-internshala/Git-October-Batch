e2e3b3af531cc90fa6bdf2a98182f16f83f68558
sonia-internshala <sonia.malik@internshala.com>
2024-12-08 16:39:26 +0530

    Add files via upload

---
+++ Conditionals.js
@@ -0,0 +1,58 @@
+//if condition-> kind of incomplete-> it tells what to do but it doesn't tell what not to do
+// let num=101;
+// if(num%2==0){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// }
+//else part is missing
+
+// var c=2;
+// var b=5;
+// var a=10;
+// if(a/b==2){
+//     c++;
+//     console.log(c);
+//     b=b-a;
+// }
+// console.log(b);
+
+
+//if else-if else-if ...    else
+// let marks=79;
+// if(marks<=30){
+//     console.log("failed the sem exams");
+// }
+// else if(marks>=31 && marks<=50){
+//     console.log("Grade D");
+// }
+// else if(marks>=51 && marks<=70){
+//     console.log("Grade C");
+// }
+// else if(marks>=71 && marks<=90){
+//     console.log("Grade B");
+// }
+// else{
+//     console.log("Grade A");
+//}
+
+
+//Ternary operators
+// (101%2==0)? console.log("Even"): console.log("Odd");
+
+let pentablet={
+    height:17,
+    width:8,
+    name:"star 03 v2",
+    color:"black",
+    hasWarranty:true
+}
+
+for( let sonia in pentablet){
+    console.log(sonia);
+}
+// console.log(pentablet.height);
+
+
+
---
+++ Operators.js
@@ -0,0 +1,56 @@
+//Arithmetic Operators
+
+// 1.Additon->adds two numbers-> +
+let num1=10;
+let num2=20;
+console.log(num1+num2);
+
+//Subtraction->subtracts two numbers-> -
+console.log(num1-num2);
+
+//Multiplication-> * -> multiplies numbers
+console.log(num1*num2);
+
+//division operators ->  /-> divides two numbers
+console.log(num1/num2);
+
+//modulo/modulus -> % ->gives you the remainder
+console.log(num1%num2);
+
+//power operator-> **-> raises a number to a power
+console.log(num1**num2);
+
+//post increment
+var x=5;
+x++;
+console.log(x);
+
+//post decrement
+var y=10;
+y--;
+console.log(y);
+
+//assignment operators
+let a=10;
+let b=20;
+console.log(a+=b);//a=a+b;
+console.log(a-=b);//a=a-b;
+console.log(a/=b);//a=a/b;
+console.log(b%=a);//b=b%a;
+console.log(a*=b);//a=a*b;
+
+//comparision or relational
+//== -> linient it only checks value
+var value1=5;//number
+var value2='5';//string
+var value3=50;
+
+console.log(value1==value2);//true
+console.log(value1===value2);//false
+console.log(value1!=value2);//false
+console.log(value1!==value2);//true
+console.log(value1>=value3);//false
+console.log(value1>value3);//false
+console.log(value1<=value3);//true
+console.log(value1<value3);//true
+
