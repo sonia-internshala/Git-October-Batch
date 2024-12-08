c0f0b26480c0111880e2a5b7bd80029b09a28236
sonia-internshala <sonia.malik@internshala.com>
2024-12-08 19:07:22 +0530

    Add files via upload

---
+++ String_array.js
@@ -0,0 +1,33 @@
+// let name1="ashok";
+// let name2='hello';
+// let name3=`world`;
+// console.log(name1);
+// console.log(name2);
+// console.log(name3);
+
+// let name1="Ashish";
+// console.log(`My name is ${name1}`);
+
+
+// console.log("Today\tis\ta\tgood\tday");
+
+let str="Australiat";
+var arr=[1,2,3,4,5,6,7];
+// console.log(str.length)//9
+// console.log(str.toUpperCase());//AUSTRALIA
+// console.log(str.toLowerCase());//australia
+// console.log(str[2]);//s
+// console.log(str.charAt(2));//s
+// console.log(str.slice(3));//tralia
+// console.log(str.slice(2,8));//strali
+// console.log(arr.splice(2,3));//3 4 5
+// console.log(str.trim());
+console.log(str.includes('q'));//false
+console.log(str.indexOf('t'));
+
+
+
+
+let s="Hello coder Coding kri kya Coder ne";
+console.log(s.replace("Coder","programmer"));//Hello programmer Coding kri kya Coder ne
+console.log(s.replaceAll("Coder","programmer"));//Hello programmer Coding kri kya programmer ne
